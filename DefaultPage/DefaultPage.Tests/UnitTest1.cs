2db7b114b907a99682ac8ce04e47c70f059b44ed
manasdalai <manas.dalai@yahoo.com>
2017-06-21 09:00:51 -0700

    Initial Commit

---
+++ DefaultPage/DefaultPage.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DefaultPage.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ DefaultPage/DefaultPage/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace DefaultPage.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
